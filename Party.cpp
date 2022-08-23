a2051f2d104d58b15b220da8894da72718e276e5
Shah Tajbid Tousif <86789646+tajbidtousif@users.noreply.github.com>
2022-08-23 20:23:20 +0600

    Add files via upload
    CF-900

---
+++ Party.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int ans=0,n,c=0;
+    cin>>n;
+    int a[n+9];
+
+    for(int i=1; i<=n; i++)
+    {
+        cin>>a[i];
+    }
+
+    for(int i=1; i<=n; i++)
+    {
+        c=0;
+        int x=a[i];
+
+        while(x != -1)
+        {
+            x=a[x];
+
+            c++;
+
+        }
+        ans=max(ans, c);
+    }
+
+    cout<<ans+1<<endl;
+
+    return 0;
+}
