85479946a251e438fadf645b30c6e32b51e05e1a
Shah Tajbid Tousif <86789646+tajbidtousif@users.noreply.github.com>
2022-08-31 14:01:10 +0600

    cf-900

---
+++ Magic Numbers.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    string s;
+    ll i;
+    bool f = true;
+
+    cin >> s;
+
+    for( i=0; i<s.length();)
+    {
+        if(s[i] == '1' && s[i+1] == '4' && s[i+2] == '4')
+        {
+            i+=3;
+        }
+        else if(s[i] =='1' && s[i+1] == '4')
+        {
+            i += 2;
+        }
+        else if(s[i] == '1')
+        {
+            i+=1;
+        }
+        else
+        {
+            f = false;
+            break;
+        }
+    }
+
+    if(f)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
